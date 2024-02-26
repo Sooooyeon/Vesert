f222759896422bef3784101593edae2f0eb81fdc
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-02-27 00:08:20 +0900

    초기설정

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/jangsooyeon/Desktop/개인프로젝트/Vesert/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
