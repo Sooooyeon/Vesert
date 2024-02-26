1d3f30bc7ff328293fe3c260123f1dada471251d
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-02-26 21:34:05 +0900

    초기설정

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Splash() {
+  return (
+    <div>
+      <Link href={'/login'}>
+        로그인
+      </Link>
+      <Link href={'/join'}>
+        회원가입
+      </Link>
+    </div>
+  );
+}
