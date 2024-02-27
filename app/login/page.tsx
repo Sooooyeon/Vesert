ea0f3abfc211831f6ee8fefeed8edbd005d19f66
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-02-27 22:08:22 +0900

    feat: 구매자 로그인

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,34 @@
 "use client"
 
 import { useState } from "react";
-import { useQuery } from 'react-query';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import Link from 'next/link';
 
 export default function Login() {
+
+  const router = useRouter();
+
   const [userId, setUserId] = useState("");  
   const [password, setPassword] = useState("");
 
-  const login = async () => {
-    const res = await fetch('https://openmarket.weniv.co.kr/accounts/login/');
-    return res.json();
-  };
+  const loginData = {
+    "username": userId,
+		"password": password,
+		"login_type": "BUYER"
+  }
 
-  const { status, data, error } = useQuery('posts', login);
-
-  console.log(status);
+  const login = async (loginData: object) => {
+  const { data } = await axios.post('https://openmarket.weniv.co.kr/accounts/login/', loginData);
   console.log(data);
-  console.log(error);
+  localStorage.setItem("token", data.token);
+  console.log(localStorage);
+  if(data.token){
+    router.push("/home")
+  }
+  return data;
+}
+
 
   const inputId = (e: React.ChangeEvent<HTMLInputElement>) => {
   setUserId(e.target.value);
@@ -28,18 +40,21 @@ export default function Login() {
     console.log(e.target.value);
   }
 
+  const handleSubmitLogin = (e: React.FormEvent<HTMLFormElement>) =>{
+    e.preventDefault();
+    login(loginData)
+  }
+
 
   return (
   <div>
     <h1>Vesert</h1>
-    <div>
-      <label htmlFor="idInput">아이디</label>
-      <input type="text" id="idInput" value={userId}/>
-    </div>
-    <div>
-      <label htmlFor="passwordInput">비밀번호</label>
-      <input type="password" id="passwordInput"/>
-    </div>
+    <form onSubmit={handleSubmitLogin}>
+      <input type="text" placeholder='아이디' onChange={inputId} value={userId}/>
+      <input type="password" placeholder='비밀번호' onChange={inputPassword} id="passwordInput"/>
+      <button>로그인</button>
+      <Link href={'/join'}><button type='button'>회원가입</button></Link>
+    </form>
   </div>
   );
 }
